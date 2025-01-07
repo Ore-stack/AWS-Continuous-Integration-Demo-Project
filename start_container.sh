f81c3eaef2e29ec4d773c9344be7633b232789d0
Lanche <oreoluwaosinowo@yahoo.com>
2025-01-07 13:53:17 +0000

    Delete start_container.sh

--- start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull biglanche/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 biglanche/simple-python-flask-app
